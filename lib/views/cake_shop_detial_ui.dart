cc087fda0212eb6e257144decb3c3c419ccda7bd
0xbedrockwm <wisanu.mangkala@gmail.com>
2026-03-06 21:08:30 +0700

    first commit

---
+++ lib/views/cake_shop_detial_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CakeShopDetialUi extends StatefulWidget {
+  const CakeShopDetialUi({super.key});
+
+  @override
+  State<CakeShopDetialUi> createState() => _CakeShopDetialUiState();
+}
+
+class _CakeShopDetialUiState extends State<CakeShopDetialUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
